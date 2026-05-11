c94e5d7748d593a76d97bd9ac9d1c787a903e8a6
Jesusgsc29 <jesusgsc29@gmail.com>
2026-05-11 16:35:33 -0400

    Initializing

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+  
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
