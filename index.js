de055e1de166f394b8dacee90150bdb1e5da308d
Jesusgsc29 <jesusgsc29@gmail.com>
2026-05-11 17:38:57 -0400

    Adding changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import * as db from './queries.js'
 const app = express()
 const port = 3000
 
@@ -13,7 +14,13 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
   })
 
-  
+
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
   })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
